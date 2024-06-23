dc7daf9a1fbcc2d77d14240aa1b4aa64225f83df
Flupinochan <flupino@metalmental.net>
2024-06-23 13:46:35 +0900

    fix

--- react/src/aws-exports.ts
+++ react/src/aws-exports.ts
@@ -1,8 +1,11 @@
+import { IdentityIdStorageKeys } from "@aws-amplify/auth/dist/esm/providers/cognito/credentialsProvider/types";
+
 const awsconfig = {
   Auth: {
     Cognito: {
       userPoolId: "us-west-2_oWOoxM9LX",
       userPoolClientId: "58knavmgv0sltabrn0kjlescoi",
+      identityPoolId: "us-west-2:81974eb0-fdf6-4b7f-8719-f9213b71eebd",
     },
   },
 };
